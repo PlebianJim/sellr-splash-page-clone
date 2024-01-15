f49f3529893ff873e69d0fa92f8e5d118488fa8d
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2024-01-15 16:09:46 +0000

    intial commit

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-let isSideNavOpen = false;
-
 function openNav() {
   document.getElementById("mySideNav").style.width = "400px";
   document.getElementById("mySideNav").style.paddingTop = "60px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  isSideNavOpen = true;
+
 }
 
 function closeNav() {
   document.getElementById("mySideNav").style.width = "0";
   document.body.style.backgroundColor = "white";
-  isSideNavOpen = false;
 }
